22590ac1e862684042330f35d78c4e1d74dd7fd6
hyeop <gksvnsfl200@naver.com>
2018-09-07 18:11:31 +0900

    hello

--- 02/app.js
+++ 02/app.js
@@ -2,14 +2,14 @@
 console.log("1번 문제 (1부터 100까지 출력)");
 
 for(var i=1; i<101; i++)
-    console.log(i+' ');
+    console.log(i);
 
 
 console.log("2번 문제 (1부터 100까지 홀수 출력)");    
 
 for(var j=1; j<101; j++){
     if(j % 2 == 1)
-        console.log(j+' ');
+        console.log(j);
 }
 
 
