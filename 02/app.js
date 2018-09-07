8a0d7ffcabd0d6dd41d8752e39c5f289ad962a24
hyeop <gksvnsfl200@naver.com>
2018-09-07 19:05:43 +0900

    Revert "과제 완료"
    This reverts commit be784b21abd5057e6742c13bed7fec10c0b7b46d.

--- 02/app.js
+++ 02/app.js
@@ -1,11 +1,11 @@
 
-//"1번 문제 (1부터 100까지 출력)";
+console.log("1번 문제 (1부터 100까지 출력)");
 
 for(var i=1; i<101; i++)
     console.log(i);
 
 
-//"2번 문제 (1부터 100까지 홀수 출력)";    
+console.log("2번 문제 (1부터 100까지 홀수 출력)");    
 
 for(var j=1; j<101; j++){
     if(j % 2 == 1)
@@ -13,7 +13,7 @@ for(var j=1; j<101; j++){
 }
 
 
-//"3번 문제 (구구단 출력)";    
+console.log("3번 문제 (구구단 출력)");    
 
 for(var k=2; k<10; k++){
     for(var n=1; n<10; n++){
@@ -22,14 +22,12 @@ for(var k=2; k<10; k++){
 }
 
 
-// "마지막 문제"
-(function(){
-    var sum = 0;
-    var result = 0;
-    for(var j=1; ;j++){    
-        result = prompt(j + '번째 숫자를 입력하세요');
-        if(!result) break;
-        sum += parseInt(result);
-    }
-    prompt("입력하신 수들의 합은 "+sum+" 입니다.");
-})();
+var result = 0;
+var box = 0;
+for(var j=1; ;j++){    
+    box = prompt(j + '번째 숫자를 입력하세요');
+    if(box != null) result += parseInt(box);
+    else if(box == null) break;
+}
+prompt("입력하신 수들의 합은 "+result+" 입니다.");
+
