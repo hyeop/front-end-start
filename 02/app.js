1f1d847c368660c62f1e39cb968ab8b30fe9085c
hyeop <gksvnsfl200@naver.com>
2018-09-07 18:09:38 +0900

    haa

--- 02/app.js
+++ 02/app.js
@@ -1,13 +1,23 @@
+
+console.log("1번 문제 (1부터 100까지 출력)");
+
 for(var i=1; i<101; i++)
-    console.log(i)
+    console.log(i+' ');
+
+
+console.log("2번 문제 (1부터 100까지 홀수 출력)");    
 
 for(var j=1; j<101; j++){
     if(j % 2 == 1)
-        console.log(j)
+        console.log(j+' ');
 }
+
+
+console.log("3번 문제 (구구단 출력)");    
 
 for(var k=2; k<10; k++){
     for(var n=1; n<10; n++){
-       console.log(k+' * '+n+' = '+k*n);
+        console.log(k+' * '+n+' = '+k*n);
     }
 }
+
