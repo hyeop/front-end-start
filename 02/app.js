8d032a2875f884fc09762f33aa5262e7a9bdbc3c
hyeop <gksvnsfl200@naver.com>
2018-09-07 18:00:33 +0900

    second project

---
+++ 02/app.js
@@ -0,0 +1,13 @@
+for(var i=1; i<101; i++)
+    console.log(i)
+
+for(var j=1; j<101; j++){
+    if(j % 2 == 1)
+        console.log(j)
+}
+
+for(var k=2; k<10; k++){
+    for(var n=1; n<10; n++){
+       console.log(k+' * '+n+' = '+k*n);
+    }
+}
